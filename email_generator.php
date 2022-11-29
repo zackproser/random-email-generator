213324c1265f7f977a7a2bf60e622297b953ccab
Muhamad Arif Md Basri <4927483+arif-basri@users.noreply.github.com>
2022-11-29 13:07:29 +0800

    convert csv to newlines

--- email_generator.php
+++ email_generator.php
@@ -1,14 +1,23 @@
 <?php
+class StreamFilterNewlines extends php_user_filter {
+    function filter($in, $out, &$consumed, $closing) {
+
+        while ( $bucket = stream_bucket_make_writeable($in) ) {
+            $bucket->data = str_replace(',', "\r\n", $bucket->data);
+            $consumed += $bucket->datalen;
+            stream_bucket_append($out, $bucket);
+        }
+        return PSFS_PASS_ON;
+    }
+}
 
 $number = $_POST['numField']; 
 $username_length = $_POST['userField']; 
 
-
-
 function generate_emails($number, $username_length) {
 if (is_numeric($number) && $number != 0) {
-	if ($number > 1000) { //put hard limit on generate request
-		$number = 1000; 
+	if ($number > 100000) { //put hard limit on generate request
+		$number = 100000; 
 	}
 	$generated_email_addresses = array(); 
 	$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
@@ -34,8 +43,11 @@ if (is_numeric($number) && $number != 0) {
 
 	$output = fopen('php://output', 'w'); 
 
+	stream_filter_register("newlines", "StreamFilterNewlines");
+	stream_filter_append($output, "newlines");
+
 	fwrite($output, "Generated $email_count random test e-mails:");  
-	fwrite($output, "    "); 
+	fwrite($output, ","); 
 	fputcsv($output, $generated_emails); 
 
 }
